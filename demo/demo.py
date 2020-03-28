391ff62ed4eeb2bc178159ef47c2989b1a337f92
Josiah Coad <josiah@coad.net>
2020-03-28 16:09:30 -0500

    cleaner code for labelling

--- demo/demo.py
+++ demo/demo.py
@@ -1,108 +1,26 @@
-# @title Import Libs
+# pylint: disable=no-member
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils, datasets
+from torchvision import transforms, models
+import torchvision
 
-from collections import defaultdict
-import matplotlib.image as mpimg
-
-import cv2
 import os
 import json
-from PIL import Image, ImageDraw, ImageFont
-import shutil
 import argparse
 
+# Load Model
+model = models.resnet18()
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 2)
 
-# @title Define Model Class
-class CNNModel(nn.Module):
-    def __init__(self):
-        super(CNNModel, self).__init__()
-
-        # Convolution 1
-        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=16,
-                              kernel_size=5, stride=1, padding=0)
-        self.relu1 = nn.ReLU()
-
-        # Max pool 1
-        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
-
-        # Convolution 2
-        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32,
-                              kernel_size=5, stride=1, padding=0)
-        self.relu2 = nn.ReLU()
-
-        # Max pool 2
-        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
-
-        # Fully connected 1 (readout)
-        self.fc1 = nn.Linear(64416, 2)
-
-    def forward(self, x):
-        # pdb.set_trace()
-        # Convolution 1
-        out = self.cnn1(x)
-        out = self.relu1(out)
-
-        # Max pool 1
-        out = self.maxpool1(out)
-
-        # Convolution 2
-        out = self.cnn2(out)
-        out = self.relu2(out)
-
-        # Max pool 2
-        out = self.maxpool2(out)
-
-        # Resize
-        out = out.view(out.size(0), -1)
-
-        # Linear function (readout)
-        out = self.fc1(out)
-
-        return out
-
-
-# @title Load Model
-model = CNNModel()
 checkpoint = torch.load('./assets/model.pth', map_location=torch.device('cpu'))
 model.load_state_dict(checkpoint['state_dict'])
 
-# @title Custom Dataset Class
 
-
-class InferenceDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        """
-        Args:
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
-        """
-        self.root_dir = root_dir
-        self.files = [x for x in os.listdir(root_dir) if x.endswith('.jpg')]
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.files)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        imgpath = os.path.join(self.root_dir, self.files[idx])
-        image = Image.open(imgpath)
-        if self.transform:
-            image = self.transform(image)
-        return image
-
-    def samples(self, idx):
-        return os.path.join(self.root_dir, self.files[idx])
-
-
-# @title Define Functions
 # 'My Drive/CCSE 689/images/myimg.png' -> myimg
 def get_basename(path):
     base = os.path.basename(path)
@@ -110,106 +28,26 @@ def get_basename(path):
     return name
 
 
-# 'My Drive/CCSE 689/images/this_is_file_11.11211.png' -> (this_is_file, 11.11211)
-def parse_imgpath(path):
-    base = get_basename(path)
-    ts = float(base.split('_')[-1])
-    vidname = '_'.join(base.split('_')[:-1])
-    return vidname, ts
-
-
 # Give probability of shouting for every image in image folder
-def predict(imgfolder):
-    # expects images to be labeled <vidname>_<ts>.ext
-    transform = transforms.Compose([transforms.Resize((144, 256)),
-                                    transforms.Grayscale(),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize(
-                                        mean=[0.485], std=[0.229]),
-                                    ])
-    dataset = InferenceDataset(imgfolder, transform)
-    dataloader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=1,
-        shuffle=False,
-        num_workers=4
-    )
-
-    predictions = defaultdict(list)
-    for i, inputs in enumerate(dataloader):
-        outputs = model(inputs)
-        prob = torch.nn.functional.softmax(outputs.data, dim=1).cpu().numpy()[
-            0][1]  # image 0, class 1
-        path = dataloader.dataset.samples(i)
-        vidname, ts = parse_imgpath(path)
-        predictions[vidname].append((ts, prob, path))
-    # sort predictions
-    predictions = {vidname:
-                   sorted(vals, key=lambda x: x[0]) for vidname, vals in predictions.items()}
-    return predictions
-
-
-# Show images annotated with probability labels
-def show_imgs(results):
-    sresults = sorted(results, key=lambda x: x[1][-7:-4])
-
-    plt.figure(figsize=(15, 15))
-    for i, (prob, img) in enumerate(sresults, 1):
-        ax = plt.subplot(10, 15, i)
-        img = mpimg.imread(img)
-        plt.imshow(img)
-        plt.title(round(prob, 2))
-        ax.set_xticks([])
-        ax.set_yticks([])
-
-    plt.show()
-
-
-# Convert video to image sequence
-def video_to_imgs(vidpath, imgpath):
-    # imgpath: folder where images should be put
-    vidname = get_basename(vidpath)
-    # Make path if it doesn't exist
-    if not os.path.exists(imgpath):
-        try:
-            os.mkdir(imgpath)
-        except:
-            raise Exception("Not able to make image folder.")
-    # play video from file
-    cap = cv2.VideoCapture(vidpath)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    assert fps > 0, 'File does not exist or is not a video format'
-    spf = 1.0 / fps  # seconds per frame
-    currentFrame = 0
-    last_ts = -1
-    curr_ts = 0
-    while(True):
-        curr_ts = currentFrame * spf
-        # capture frame-by-frame
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        if curr_ts - last_ts > .3:  # only sample  ~3 fps
-            last_ts = curr_ts
-            # save image of the current frame in jpg file
-            name = '{}_{:5f}.jpg'.format(os.path.join(
-                imgpath, vidname), currentFrame * spf)
-            cv2.imwrite(name, frame)
-
-        currentFrame += 1
-
-    # release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+def predict(video):
+    # [D, W, H, C] -> [D, C, W, H]
+    frames = torch.transpose(torch.transpose(video, 1, 2), 1, 3)
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((144, 256)),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    transformed = torch.stack([transform(frame) for frame in frames])
+    out = model(transformed)
+    return torch.nn.functional.softmax(out.data, dim=1).numpy().T[1]
 
 
 # make time-probability graph
 def mk_prob_graph(predictions, title, outfolder):
-    # predictions: [(ts, prob)]
-    timestamps = [x[0] for x in predictions]
-    probs = [x[1] for x in predictions]
-    plt.plot(timestamps, probs, marker='.')
+    # predictions: [[ts, prob], ...]
+    plt.plot(*predictions.T, marker='.')
     plt.title(title)
     plt.xlabel('seconds')
     plt.ylim(0, 1)
@@ -217,93 +55,65 @@ def mk_prob_graph(predictions, title, outfolder):
     plt.savefig(os.path.join(outfolder, title + '.png'))
 
 
-def mk_prob_graphs(predictions, outfolder):
-    for vidname, preds in predictions.items():
-        mk_prob_graph(preds, vidname, outfolder)
-
-
 # make json file
 def mk_json(predictions, title, outfolder):
-    data = {'shouting': [[ts, float(prob)] for ts, prob, _ in predictions]}
+    data = {'shouting': [[ts, float(prob)] for ts, prob in predictions]}
     with open(os.path.join(outfolder, title + '.json'), 'w') as f:
         f.write(json.dumps(data))
 
 
-def mk_jsons(predictions, outfolder):
-    for vidname, preds in predictions.items():
-        mk_json(preds, vidname, outfolder)
+# make gif with predictions
+def mk_gif(predictions, frames, spf, title, outfolder):
+    fig = plt.figure(figsize=(13, 10))
+    ims = []
+    for img, (_, prob) in zip(frames, predictions):
+        im = plt.imshow(img, animated=True)
+        plt.axis('off')
+        t = plt.text(100, 100, round(prob, 2), color='red', fontsize=20)
+        ims.append([im, t])
+    anim = animation.ArtistAnimation(fig, ims, interval=spf*1000, blit=True)
+    anim.save(os.path.join(outfolder, title + '.mp4'))
+    plt.close()
 
 
-# @title Make gifs
-def annotate_img(imgpath, prob):
-    # get an image
-    base = Image.open(imgpath).convert('RGBA')
-
-    # make a blank image for the text, initialized to transparent text color
-    txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
-
-    # get a font
-    fnt = ImageFont.truetype(
-        './assets/Roboto-Regular.ttf', 40)
-
-    # get a drawing context
-    d = ImageDraw.Draw(txt)
-
-    # draw text, full opacity
-    d.text((10, 60), '{:.2f}'.format(
-        prob), font=fnt, fill=(255, 0, 0, 255))
-
-    return Image.alpha_composite(base, txt)
+def downsample(vid, orig_fps, dest_fps):
+    assert orig_fps >= dest_fps
+    skip = orig_fps // dest_fps
+    return torch.stack([frame for i, frame in enumerate(vid) if i % skip == 0])
 
 
-def mk_gif(predictions, title, outfolder):
-    images = [annotate_img(imgpath, prob) for _, prob, imgpath in predictions]
-    duration = (predictions[1][0] - predictions[0][0]) * 1000
-    images[0].save(os.path.join(outfolder, title + '.gif'),
-                   save_all=True, append_images=images[1:], optimize=False, duration=duration, loop=0)
-
-
-def mk_gifs(predictions, outfolder):
-    # predictions: {(path, vidname): [(ts, prob)]}
-    for vidname, preds in predictions.items():
-        mk_gif(preds, vidname, outfolder)
-
-
-def labelvid(vidpath, imgpath, resultspath):
-    """
-    (vidpath: Path to the video file to analyze
-    imgpath: Path to the folder to put the pictures from the video
-    resultspath: Path to the folder to put the results of the predictions
-    """
-    # Convert video to images
-    video_to_imgs(vidpath, imgpath)
+# get prediction results for video
+def labelvid(vidpath, resultspath):
     # Use model to predict for each image in seqence
-    print(f"Img creation done")
-    predictions = predict(imgpath)
-    print(f"Prediction done")
-    # Create graph and json file
-    mk_prob_graphs(predictions, resultspath)
-    mk_jsons(predictions, resultspath)
-    mk_gifs(predictions, resultspath)
-    print(f"Results finished")
-    # remove temp img folder
-    shutil.rmtree(imgpath)
+    assert os.path.exists(vidpath), f'{vidpath} does not exist'
+    video, _, data = torchvision.io.read_video(vidpath, pts_unit='sec')
+    orig_fps = data['video_fps']
+    dest_fps = 3
+    spf = 1 / dest_fps
+    print(f'video read ({len(video)} frames)')
+    video = downsample(video, orig_fps, dest_fps)
+    print(f'video downsampled ({len(video)} frames)')
+    probs = predict(video)
+    predictions = np.array([(i * spf, p) for i, p in enumerate(probs)])
+    print('predictions finished')
+    # Create results
+    vidname = get_basename(vidpath)
+    mk_prob_graph(predictions, vidname, resultspath)
+    mk_json(predictions, vidname, resultspath)
+    mk_gif(predictions, video, spf, vidname, resultspath)
 
 
-# get vidpath, imgpath, resultspath from command line if provided, else default
+# get vidpath, resultspath from command line if provided, else default
 def parseargs():
     parser = argparse.ArgumentParser(
         description='Demo for shouting action recognition in videos.')
     parser.add_argument('--vidpath', help='The path to an .mp4 file.')
     parser.add_argument(
-        '--imgpath', help='The path to a folder to store temp data. Parent directory should exist.')
-    parser.add_argument(
         '--resultspath', help='The path to a folder to store the results of analysis.')
     args = parser.parse_args()
     vidpath = args.vidpath or './videos/demo.mov'
-    imgpath = args.imgpath or './tmp'
     resultspath = args.resultspath or './results'
-    return vidpath, imgpath, resultspath
+    return vidpath, resultspath
 
 
 if __name__ == '__main__':
@@ -311,8 +121,5 @@ if __name__ == '__main__':
         description='Demo for shouting action recognition in videos.')
     parser.add_argument('--vidpath', help='The path to an .mp4 file.')
     parser.add_argument(
-        '--imgpath', help='The path to a folder to store temp data. Parent directory should exist.')
-    parser.add_argument(
         '--resultspath', help='The path to a folder to store the results of analysis.')
-    args = parser.parse_args()
     labelvid(*parseargs())
