eda73f72b2c23c30f2ca011c08394ed356c24bc1
Josiah Coad <josiah@coad.net>
2020-04-10 16:44:37 -0500

    changes for openpose

--- demo/demo.py
+++ demo/demo.py
@@ -1,75 +1,20 @@
-# pylint: disable=no-member
+from utils import get_frame_probs, get_fps, mk_pred_plot, mk_pred_json
+from tensorflow import keras
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
-import torch
-import torch.nn as nn
-from torchvision import transforms, models
-import torchvision
-
-import os
-import json
-import argparse
-
-from utils import *
-
-# Load Model
-# Load face detector
-mtcnn = MTCNN(margin=14, keep_all=True, factor=0.5, device=device).eval()
-
-# Load facial recognition model
-resnet = InceptionResnetV1(pretrained='vggface2', device=device).eval()
-
-# 'My Drive/CCSE 689/images/myimg.png' -> myimg
-def get_basename(path):
-    base = os.path.basename(path)
-    name, _ = os.path.splitext(base)
-    return name
 
 
-# Give probability of shouting for every image in image folder
-def predict(video):
-    # [D, W, H, C] -> [D, C, W, H]
-    frames = torch.transpose(torch.transpose(video, 1, 2), 1, 3)
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((144, 256)),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    transformed = torch.stack([transform(frame) for frame in frames])
-    out = model(transformed)
-    return torch.nn.functional.softmax(out.data, dim=1).numpy().T[1]
 
-
-# make time-probability graph
-def mk_prob_graph(predictions, title, outfolder):
-    # predictions: [[ts, prob], ...]
-    plt.plot(*predictions.T, marker='.')
-    plt.title(title)
-    plt.xlabel('seconds')
-    plt.ylim(0, 1)
-    plt.ylabel('shouting probability')
-    plt.savefig(os.path.join(outfolder, title + '.png'))
-
-
-# make json file
-def mk_json(predictions, title, outfolder):
-    data = {'shouting': [[ts, float(prob)] for ts, prob in predictions]}
-    with open(os.path.join(outfolder, title + '.json'), 'w') as f:
-        f.write(json.dumps(data))
-
-def labelvid(vidpath, resultspath):
-    facelist, frames, timestamps = detection_pipeline(vidpath)
-    print("Video processed")
-    probs = [0 if faces is None else get_frame_prob(faces) for faces in facelist]
-    print("Predictions done")
+def main(vidpath, resultspath):
+    model = keras.models.load_model('models/keras_ffn_personal.h5')
+    probs = get_frame_probs('openpose_json/personal/shout/1/*', model)
+    fps = get_fps(vidpath)
+    ts = fps * np.arange(len(probs))
+    predictions = np.hstack(ts, probs)
     title = get_basename(vidpath)
-    show_frames(frames, probs, 5, outfolder=resultspath, title=title)
-    show_timestamps(probs, timestamps, outfolder=resultspath, title=title)
-    mk_json(zip(timestamps, probs), title, resultspath)
+    mk_pred_plot(predictions, resultspath, title)
+    mk_pred_json(predictions, resultspath, title)
+
+
 
 # get vidpath, resultspath from command line if provided, else default
 def parseargs():
@@ -90,4 +35,5 @@ if __name__ == '__main__':
     parser.add_argument('--vidpath', help='The path to an .mp4 file.')
     parser.add_argument(
         '--resultspath', help='The path to a folder to store the results of analysis.')
-    labelvid(*parseargs())
+    vidpath, resultspath = parseargs()
+    main(vidpath, resultspath)
--- demo/utils.py
+++ demo/utils.py
@@ -1,167 +1,100 @@
-import glob
-import time
-import torch
+from mlxtend.image import extract_face_landmarks
 import cv2
-from PIL import Image
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
 import os
-import pickle
-from joblib import dump, load
-from sklearn.decomposition import PCA
-
-# See github.com/timesler/facenet-pytorch:
-from facenet_pytorch import MTCNN, InceptionResnetV1, extract_face
-
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-print(f'Running on device: {device}')
-
-# Load face detector
-mtcnn = MTCNN(margin=14, keep_all=True, factor=0.5, device=device).eval()
-
-# Load facial recognition model
-resnet = InceptionResnetV1(pretrained='vggface2', device=device).eval()
-
-class DetectionPipeline:
-    """Pipeline class for detecting faces in the frames of a video file."""
-    
-    def __init__(self, detector, n_frames=None, batch_size=60, resize=None):
-        """Constructor for DetectionPipeline class.
-
-        Keyword Arguments:
-            n_frames {int} -- Total number of frames to load. These will be evenly spaced
-                throughout the video. If not specified (i.e., None), all frames will be loaded.
-                (default: {None})
-            batch_size {int} -- Batch size to use with MTCNN face detector. (default: {32})
-            resize {float} -- Fraction by which to resize frames from original prior to face
-                detection. A value less than 1 results in downsampling and a value greater than
-                1 result in upsampling. (default: {None})
-        """
-        self.detector = detector
-        self.n_frames = n_frames
-        self.batch_size = batch_size
-        self.resize = resize
-    
-    def __call__(self, filename):
-        """Load frames from an MP4 video and detect faces.
-
-        Arguments:
-            filename {str} -- Path to video.
-        """
-        assert os.path.exists(filename)
-        # Create video reader and find length and fps
-        v_cap = cv2.VideoCapture(filename)
-        v_len = int(v_cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        fps = v_cap.get(cv2.CAP_PROP_FPS)
-
-        # Pick 'n_frames' evenly spaced frames to sample
-        if self.n_frames is None:
-            sample = np.arange(0, v_len)
-        else:
-            sample = np.linspace(0, v_len - 1, self.n_frames).astype(int)
-
-        # Loop through frames
-        faces = []
-        frames = []
-        batch = []
-        for j in range(v_len):
-            success = v_cap.grab()
-            if j in sample:
-                # Load frame
-                success, frame = v_cap.retrieve()
-                if not success:
-                    continue
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                frame = Image.fromarray(frame)
-                
-                # Resize frame to desired size
-                if self.resize is not None:
-                    frame = frame.resize([int(d * self.resize) for d in frame.size])
-                batch.append(frame)
-
-                # When batch is full, detect faces and reset frame list
-                if len(batch) % self.batch_size == 0 or j == sample[-1]:
-                    faces.extend(self.detector(batch))
-                    batch = []
-                frames.append(frame)
-
-        v_cap.release()
-
-        return faces, frames, sample * (1 / fps)
+import glob
+import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# Define face detection pipeline
-detection_pipeline = DetectionPipeline(detector=mtcnn, batch_size=60, resize=0.25)
-
-def load_model(load_file=None, X=None, y=None):
-  return pickle.loads(load(f'{load_file}.joblib'))
-
-
-clf = load_model('assets/model_svm_personal')
-pca = load_model('assets/model_pca_personal')
-
-def process_faces(faces, resnet):
-    # Filter out frames without faces
-    faces = [f for f in faces if f is not None]
-    if len(faces) == 0:
-      return torch.Tensor([])
-    faces = torch.cat(faces).to(device)
-
-    # Generate facial feature vectors using a pretrained model
-    embeddings = resnet(faces)
-
-    # Calculate centroid for video and distance of each face's feature vector from centroid
-    centroid = embeddings.mean(dim=0)
-    x = (embeddings - centroid).norm(dim=1).cpu()
-    
-    return embeddings.cpu()
+# 'My Drive/CCSE 689/images/myimg.png' -> myimg
+def get_basename(path):
+    base = os.path.basename(path)
+    name, _ = os.path.splitext(base)
+    return name
 
 
-def scale(x):
-  return (x - x.min()) / (x.max() - x.min())
+def get_fps(path):
+    return cv2.VideoCapture(path).get(cv2.CAP_PROP_FPS)
 
 
-#@title show_face_preds
-def predict_face_probs(facelist, resnet, pca, clf):
-  # get 512 features from resnet
-  with torch.no_grad():
-    features = process_faces(facelist, resnet)
-  # reduce to 20 features using PCA
-  embeddings = pca.transform(features)
-  # use svm on 20 features to predict positive probability
-  return clf.predict_proba(embeddings).T[1]
-
-
-def show_timestamps(probs, timestamps, outfolder, title):
-  probs = pd.Series(probs).ewm(span=10).mean().values
-  plt.plot(timestamps, probs)
-  plt.ylim(0, 1)
-  path = os.path.join(outfolder, title + '.png')
-  print('Timestamps being saved at ' + path)
-  plt.savefig(path)
-  plt.close()
+def vid2frames(path):
+    # returns: [(W,H,C), ...]
+    cap = cv2.VideoCapture(path)
+    frames = []
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        frames.append(frame)
+    cap.release()
+    cv2.destroyAllWindows()
+    return frames
 
 
 def show_frames(frames, probs, nskip=1, outfolder=None, title=None):
-  print(f'Probability video is shouting: {round(np.mean([p for p in probs if p]), 2)}')
-  probs = pd.Series(probs).ewm(span=10).mean().values
-  numimgs = len(frames)
-  ncols = 10
-  nrows = np.ceil(numimgs/(ncols * nskip))
-  plt.figure(figsize=(ncols, nrows))
-  for i, (frame, prob) in enumerate(zip(frames[0:numimgs:nskip], probs[0:numimgs:nskip])):
-    plt.subplot(nrows, ncols, i+1)
-    plt.title(f'{round(prob, 2)}')
-    plt.imshow(np.array(frame))
-    plt.axis('off')
-  plt.tight_layout()
-  path = os.path.join(outfolder, title + '_frames.png')
-  print('Frames being saved at ' + path)
-  plt.savefig(path)
-  plt.close()
+    print(
+        f'Probability video is shouting: {round(np.mean([p for p in probs if p]), 2)}')
+    numimgs = len(frames)
+    ncols = 10
+    nrows = np.ceil(numimgs/(ncols * nskip))
+    plt.figure(figsize=(ncols, nrows))
+    for i, (frame, prob) in enumerate(zip(frames[0:numimgs:nskip], probs[0:numimgs:nskip])):
+        plt.subplot(nrows, ncols, i+1)
+        plt.title(f'{round(prob, 2)}')
+        plt.imshow(frame)
+        plt.axis('off')
+    plt.tight_layout()
+    path = os.path.join(outfolder, title + '_frames.png')
+    print('Frames being saved at ' + path)
+    plt.savefig(path)
+    plt.close()
 
 
-def get_frame_prob(faces):
-  faceprobs = predict_face_probs([faces], resnet, pca, clf)
-  return max(faceprobs) # return prob of at least one person shouting in video
+def intermix(lst1, lst2):
+    return np.array([[i, j] for i, j in zip(lst1, lst2)]).ravel()
+
+
+def scale01(x):
+    return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+
+def get_scaled_points(face):
+    x, y, c = np.array(np.split(face, 70)).T
+    if np.mean(c) > 0.2 and (min(x) == 0 or max(x) == 0 or min(y) == 0 or max(y) == 0):
+        show_face(intermix(x, y))
+    return intermix(scale01(x), scale01(y)) if np.mean(c) > 0.2 else None
+
+
+def get_frame_probs(path, clf):
+    files = glob.glob(path)
+    faceframes = []
+    for i, name in enumerate(files):
+        with open(name) as file:
+            js = json.loads(file.read())
+            if len(js['people']) > 0:
+                faceframes.append(js['people'][0]['face_keypoints_2d'])
+            else:
+                faceframes.append(None)
+    faceframes = [None if frame is None else get_scaled_points(
+        np.array(frame)) for frame in faceframes]
+
+    def pred(x): return clf.predict(x.reshape(1, -1)).T[1][0]
+    probs = [np.NaN if frame is None else pred(frame) for frame in faceframes]
+    return probs
+
+
+### --------- OUTPUTS ---------- ###
+
+def mk_pred_plot(predictions, outfolder, title):
+    plt.plot(*predictions.T)
+    plt.ylim(-0.1, 1.1)
+    path = os.path.join(outfolder, title + '.png')
+    print('Timestamps being saved at ' + path)
+    plt.savefig(path)
+    plt.close()
+
+
+def mk_pred_json(predictions, outfolder, title):
+    with open(os.path.join(outfolder, title + '.json'), 'w') as f:
+        f.write(json.dumps({'shouting': predictions}))
