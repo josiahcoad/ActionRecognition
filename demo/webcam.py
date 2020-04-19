e2c2e62f6ee92df2fc6b8298fcdd4857b7b270b1
Josiah Coad <josiah@coad.net>
2020-04-19 09:41:58 -0500

    webcap updates

--- demo/webcam.py
+++ demo/webcam.py
@@ -4,9 +4,14 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    bgra = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    rbg = cv2.cvtColor(frame, cv2.COLOR_BGR2RBG)
+    frames = frame.reshape(1, *frame.shape)
+    points = frames2points(frames)
+    x = preprocess(points)[0]
+    prob = singlepred(model, x)
 
-    cv2.imshow('frame', rgb)
+    cv2.imshow('frame', bgra)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         out = cv2.imwrite('capture.jpg', frame)
         break
